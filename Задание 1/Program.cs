add4b4d46f4511eb5d2caf965eb77432dad583ca
dotNETBeginner <ukanton1219@gmail.com>
2019-09-03 16:04:09 +0300

    Добавил решение первого практического задания

---
+++ Задание 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Задание_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DarkGray");
+        }
+    }
+}
