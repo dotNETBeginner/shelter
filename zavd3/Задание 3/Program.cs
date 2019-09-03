d3f7457c2db44fd5f990e2ef226fe7f914af828e
dotNETBeginner <ukanton1219@gmail.com>
2019-09-03 17:18:19 +0300

    Create Program.cs

---
+++ zavd3/Задание 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Задание_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n0 = 1.60217657d; 
+            double n1 = 3.141592653589793238d;
+            decimal n2 = 7.8184261974584555216535342341m;
+            Console.WriteLine($"{n0} \n {n1} \n {n2}");
+        }
+    }
+}
