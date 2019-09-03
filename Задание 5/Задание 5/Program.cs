aaa65718dc0a20695c0e361648f19592203171ce
dotNETBeginner <ukanton1219@gmail.com>
2019-09-03 17:47:27 +0300

    5 задание

---
+++ Задание 5/Задание 5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Задание_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c, average;
+            Console.WriteLine("Введите 3 числа \n");
+            a=Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            average = (a + b + c) / 3.0f;
+            Console.WriteLine(average);
+        }
+    }
+}
