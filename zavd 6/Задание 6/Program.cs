eed15b7be028dd625f5fee5322ab43144bd2863e
dotNETBeginner <ukanton1219@gmail.com>
2019-09-03 18:08:55 +0300

    Задание 6

---
+++ zavd 6/Задание 6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Задание_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, h, Area;
+                Console.WriteLine("Введите а");
+            a = float.Parse(Console.ReadLine());
+                Console.WriteLine("Введите b");
+            b = float.Parse(Console.ReadLine());
+                Console.WriteLine("Введите h");
+            h = float.Parse(Console.ReadLine());
+            Area = ((a + b) / 2) * h;
+            Console.WriteLine($"Площадь - {Area}");
+        }
+    }
+}
