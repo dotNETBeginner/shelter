34b8efa2a5061ac48bd27a731ec81923fe940341
dotNETBeginner <ukanton1219@gmail.com>
2019-09-03 17:29:24 +0300

    4 задание

---
+++ zavd 4/Задание 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Задание_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word0 = "Software University";
+            char word1 = 'B';
+            char word2 = 'y';
+            char word3 = 'e';
+            string word4 = "I love programming";
+            Console.WriteLine($"{word0} \n {word1} \n {word2} \n {word3} \n {word4}");
+        }
+    }
+}
