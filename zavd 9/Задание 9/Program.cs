93e58ef3f64178f3b28f3c8122e3957800567b58
dotNETBeginner <ukanton1219@gmail.com>
2019-09-04 15:47:12 +0300

    9 задание

---
+++ zavd 9/Задание 9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Задание_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float number, min;
+            Console.WriteLine("Введите число");
+            number = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите минимальное число для прохода");
+            min = float.Parse(Console.ReadLine());
+            if (number % 2 != 0 && number > min)
+                   {
+                Console.Write($"Заданое вами число является не парным, и оно больше {min}");
+                }
+            if (number % 2 != 0 && number < min)
+            {
+                Console.Write($"Заданое вами число является не парным, но оно меньше {min}");
+            }
+            if (number % 2 == 0 && number > min)
+            {
+                Console.Write($"Заданое вами число является парным, но оно хотя бы больше {min}");
+            }
+            if (number % 2 == 0 && number < min)
+            {
+                Console.Write($"Заданое вами число является парным, и оно меньше {min}");
+            }
+        }
+    }
+}
