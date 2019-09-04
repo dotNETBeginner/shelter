8ab9b3b0986dc0f5658c15f1114e1b78fbc64444
dotNETBeginner <ukanton1219@gmail.com>
2019-09-04 15:08:04 +0300

    8 задание

---
+++ zavd 8/Задание 8/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Задание_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number, n, nDigit;
+            Console.WriteLine("Введите число");
+            number=int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите порядковый номер цифры числа, который хотите увидеть на экране (Нумерация идёт справа на лево)");
+            n = int.Parse(Console.ReadLine());
+            nDigit =Convert.ToInt32((number/Math.Pow(10,n-1))%10);
+            Console.Write($"Результат - {nDigit-1}");
+
+        }
+    }
+}
