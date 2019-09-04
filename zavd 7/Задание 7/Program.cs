40f62afdf758ab6ce199833e9888a3cf4b20bbc3
dotNETBeginner <ukanton1219@gmail.com>
2019-09-04 14:11:13 +0300

    7 задание

---
+++ zavd 7/Задание 7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Задание_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, lastDigit;
+            Console.WriteLine("Введите n");
+            n = int.Parse(Console.ReadLine());
+            lastDigit = n % 10;
+            Console.Write($"Last digit of n is\n{lastDigit}");
+        }
+    }
+}
