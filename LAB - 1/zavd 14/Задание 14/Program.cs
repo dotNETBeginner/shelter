258303b38285e2b9f867c68e9dd456e8c9f68251
dotNETBeginner <ukanton1219@gmail.com>
2019-09-05 15:00:18 +0300

    .

---
+++ LAB - 1/zavd 14/Задание 14/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Задание_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,n1=1;
+            Console.WriteLine("Введите n");
+            n = int.Parse(Console.ReadLine());
+            for (int i=2;i<=n;i++)
+                 n1*= i;
+            n = n1;
+            Console.Write($"n! - {n}");
+        }
+    }
+}
