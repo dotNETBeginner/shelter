61bbe39e621ccc8ae139e6f266ceba6adabd6983
dotNETBeginner <ukanton1219@gmail.com>
2019-09-05 14:47:14 +0300

    Задание 12

---
+++ zavd 12/Задание 13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Задание_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c, k=0;
+            Console.WriteLine("Input a");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Input b");
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Input c");
+            c = float.Parse(Console.ReadLine());
+            if (a < 0)
+                k++;
+            if (b < 0)
+                k++;
+            if (c < 0)
+                k++;
+            if(k%2==0)
+                Console.Write($"Произведение чисел {a} {b} {c} - Позитивное");
+            else
+                Console.Write($"Произведение чисел  {a}  {b}  {c}  - Негативное");
+        }
+    }
+}
