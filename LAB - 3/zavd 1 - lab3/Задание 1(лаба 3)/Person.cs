918cc6546c8bffbcd0a248eaf3831b54a0a886f7
dotNETBeginner <ukanton1219@gmail.com>
2019-09-17 14:29:32 +0300

    1 задание

---
+++ LAB - 3/zavd 1 - lab3/Задание 1(лаба 3)/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_1_лаба_3_
+{
+    class Person
+    {
+        private string name;
+        private int age;
+
+        public Person() { }
+
+        public Person(int Age, string Name)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+
+        public void printInfo()
+            {
+            Console.WriteLine($"{Name} is {Age} years old.");
+             }
+
+    }
+}
