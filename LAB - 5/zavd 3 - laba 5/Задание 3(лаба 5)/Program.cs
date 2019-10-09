4c5ac1900c936262873a687490f6422fecb5e900
dotNETBeginner <ukanton1219@gmail.com>
2019-10-09 15:57:26 +0300

    3 задание 5 лаба

---
+++ LAB - 5/zavd 3 - laba 5/Задание 3(лаба 5)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Задание_3_лаба_5_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Имя и возраст курицы.");
+            string[] s = Console.ReadLine().Split(" ");
+
+            Chicken someChiken = new Chicken(s[0],int.Parse(s[1]));
+
+            someChiken.printInfo();
+        }
+    }
+}
