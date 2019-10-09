195b6989115a026fd50ff45d0ec0b7bfd2ac713e
dotNETBeginner <ukanton1219@gmail.com>
2019-10-09 15:35:47 +0300

    2 задание 5 лаба

---
+++ LAB - 5/zavd 2 - laba 5/Задание 1(лаба 5)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Задание_1_лаба_5_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите параметры фигуры в таком порядке - *Длинна  Ширина  Высота*");
+            string[] s = Console.ReadLine().Split(" ");
+
+            Figure someFigure = new Figure(float.Parse(s[0]), float.Parse(s[1]), float.Parse(s[2]));
+
+            someFigure.findSomeProperties();
+        }
+    }
+}
