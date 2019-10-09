89100d1b0aa1ef2931c06b6ea65ee3d24df6fff5
dotNETBeginner <ukanton1219@gmail.com>
2019-10-09 19:19:17 +0300

    4 задание 5 лаба

---
+++ LAB - 5/zavd 4 - laba 5/Задание 4(лаба 5)/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_4_лаба_5_
+{
+    class Product
+    {
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set
+            {
+                if (value == "" || value == " ") { productName = null; }
+                else { productName = value; }
+            }
+        }
+        public int ProductPrice { get; set; }
+
+        public Product() { }
+
+        public Product(int i, string[] s)
+        {
+            ProductName = s[i];
+            ProductPrice = int.Parse(s[i + 1]);
+        }
+
+        public Product(string name,int price)
+        {
+            ProductName = name;
+            ProductPrice = price;
+        }
+    }
+}
