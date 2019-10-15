5af80a17cac9062634a906335d8623e69ecf5e25
dotNETBeginner <ukanton1219@gmail.com>
2019-10-15 15:58:22 +0300

    2 задание 6 лаба

---
+++ LAB - 6/zavd 2 - lab 6/Задание 2(лаба 6)/Book.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_2_лаба_6_
+{
+    public class Book
+    {
+        private string author;
+        private string title;
+        private float price;
+
+        public string Author
+        {
+            get { return author; }
+            set
+            {
+                string someProp;
+                bool isDigit=false;
+
+                for (int i=0;i<value.Length;i++)
+                {
+                    if(value[i] == ' ')
+                    {
+                        someProp = Convert.ToString(value[i+1]);
+                        isDigit = int.TryParse(someProp,out int n);
+                        break;
+                    }
+                }
+
+                if(isDigit)
+                { throw new ArgumentException("Author not valid!"); }
+                else { author = value; }
+            }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if(value.Length < 3)
+                { throw new ArgumentException("Title not valid!"); }
+                else { title = value; }
+            }
+        }
+
+        public float Price
+        {
+            get { return price; }
+            set
+            {
+                if(value < 0 || value == 0)
+                { throw new ArgumentException("Price not valid!"); }
+                else { price = value; }
+            }
+        }
+
+        public Book(string author,string title,float price)
+        {
+            Author = author;
+            Title = title;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder someString = new StringBuilder();
+            someString.Append($"Type: {GetType().Name}\nTitle: {Title}\nAuthor: {Author}\nPrice: {Price.ToString("0.00")}");
+
+            return someString.ToString();
+        }
+    }
+}
---
+++ LAB - 6/zavd 2 - lab 6/Задание 2(лаба 6)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Задание_2_лаба_6_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Input author of book, title of book, and price of book");
+                string authorOfBook = Console.ReadLine();
+                string titleOfBook = Console.ReadLine();
+                float priceOfBook = float.Parse(Console.ReadLine());
+
+                Book someBook = new Book(authorOfBook, titleOfBook, priceOfBook);
+                GoldenEditionBook someGoldenEditionBook = new GoldenEditionBook(authorOfBook, titleOfBook, priceOfBook);
+
+                Console.WriteLine(someBook.ToString());
+                Console.WriteLine();
+                Console.WriteLine(someGoldenEditionBook.ToString());
+            }
+            catch (ArgumentException ae)
+            { Console.WriteLine(ae.Message); }
+        }
+    }
+}
