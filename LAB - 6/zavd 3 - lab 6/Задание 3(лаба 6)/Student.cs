16b78d45b2bfab1069adb40a913b603c99ab709c
dotNETBeginner <ukanton1219@gmail.com>
2019-10-15 17:32:16 +0300

    3 задание 6 лаба

---
+++ LAB - 6/zavd 3 - lab 6/Задание 3(лаба 6)/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_3_лаба_6_
+{
+    class Student : Human
+    {
+        private string facultyNumber;
+
+        public string FacultyNumber
+        {
+            get { return facultyNumber; }
+            set
+            {
+                if(value.Length < 5 || value.Length > 10)
+                { throw new ArgumentException("Invalid faculty number!"); }
+                else { facultyNumber = value; }
+            }
+        }
+
+        public Student(string firstName, string lastName, string facultyNumber)
+            :base(firstName,lastName)
+        {
+            FacultyNumber = facultyNumber;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Faculty Number: {FacultyNumber}");
+        }
+    }
+}
