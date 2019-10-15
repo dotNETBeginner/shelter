9c7ca7080613ee785822a766f522815640b702e3
dotNETBeginner <ukanton1219@gmail.com>
2019-10-15 14:19:33 +0300

    1 задание 6 лаба

---
+++ LAB - 6/zavd 1 - lab 6/Задание 1(лаба 6)/Child.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_1_лаба_6_
+{
+    class Child : Person
+    {
+        public override int Age
+        {
+            get { return base.Age; }
+            set
+            {
+                if(value > 15)
+                { throw new ArgumentException("Child`s age must be less than 15!"); }
+                else { base.Age = value; }
+            }
+        }
+
+        public Child(string name,int age)
+            :base(name,age)
+        {
+        }
+    }
+}
---
+++ LAB - 6/zavd 1 - lab 6/Задание 1(лаба 6)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Задание_1_лаба_6_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input name and age of Person.");
+            string[] s = Console.ReadLine().Split(" ");
+
+            Person somePerson = new Person(s[0],int.Parse(s[1]));
+
+            Console.WriteLine("Input name and age of Child.");
+             s = Console.ReadLine().Split(" ");
+
+            Child someChild = new Child(s[0],int.Parse(s[1]));
+
+            Console.WriteLine(somePerson.ToString());
+            Console.WriteLine(someChild.ToString());
+        }
+    }
+}
