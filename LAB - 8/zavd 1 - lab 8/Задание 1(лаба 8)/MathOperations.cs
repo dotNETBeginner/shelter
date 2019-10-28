046d808e2b920cffc3e463b66113d59b1ad44586
dotNETBeginner <ukanton1219@gmail.com>
2019-10-28 15:59:02 +0300

    1 задание 8 лаба

---
+++ LAB - 8/zavd 1 - lab 8/Задание 1(лаба 8)/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_1_лаба_8_
+{
+    class MathOperations
+    {
+        public MathOperations() { }
+
+        public virtual int Add(int a, int b)
+        { return a + b; }
+
+        public virtual double Add(double a, double b, double c)
+        { return a + b + c; }
+
+        public virtual decimal Add(decimal a, decimal b, decimal c)
+        { return a + b + c; }
+    }
+}
---
+++ LAB - 8/zavd 1 - lab 8/Задание 1(лаба 8)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Задание_1_лаба_8_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathOperations someOperations = new MathOperations();
+
+            Console.WriteLine(someOperations.Add(2,3));
+            Console.WriteLine(someOperations.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(someOperations.Add(2.2m, 3.3m, 4.4m));
+        }
+    }
+}
