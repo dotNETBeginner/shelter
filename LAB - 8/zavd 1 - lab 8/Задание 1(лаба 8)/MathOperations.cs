d14eddabe4cf8bacddba2bc3ff99edefec0369f2
dotNETBeginner <ukanton1219@gmail.com>
2019-10-28 16:06:54 +0300

    /

--- LAB - 8/zavd 1 - lab 8/Задание 1(лаба 8)/MathOperations.cs
+++ LAB - 8/zavd 1 - lab 8/Задание 1(лаба 8)/MathOperations.cs
@@ -8,13 +8,13 @@ namespace Задание_1_лаба_8_
     {
         public MathOperations() { }
 
-        public virtual int Add(int a, int b)
+        public int Add(int a, int b)
         { return a + b; }
 
-        public virtual double Add(double a, double b, double c)
+        public double Add(double a, double b, double c)
         { return a + b + c; }
 
-        public virtual decimal Add(decimal a, decimal b, decimal c)
+        public decimal Add(decimal a, decimal b, decimal c)
         { return a + b + c; }
     }
 }
