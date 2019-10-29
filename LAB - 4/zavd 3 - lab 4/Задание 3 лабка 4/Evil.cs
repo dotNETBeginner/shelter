7f9968106c3848c14d89796003d3bd501c9c3932
dotNETBeginner <ukanton1219@gmail.com>
2019-10-29 16:56:56 +0300

    3 задание 4 лаба

---
+++ LAB - 4/zavd 3 - lab 4/Задание 3 лабка 4/Evil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_3_лаба_4_
+{
+    public class Evil : ICoordinable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Evil(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Move(int[,] someMatrix)
+        {
+            while (X >= 0 && Y >= 0)
+            {
+                if (X >= 0 && X < someMatrix.GetLength(0) && Y >= 0 && Y < someMatrix.GetLength(1))
+                {
+                    someMatrix[X, Y] = 0;
+                }
+                X--;
+                Y--;
+            }
+        }
+    }
+}
---
+++ LAB - 4/zavd 3 - lab 4/Задание 3 лабка 4/ICoordinable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_3_лаба_4_
+{
+    interface ICoordinable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Move(int[,] matrix);
+    }
+}
