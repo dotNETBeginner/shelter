1a9880035aebc1d4d790da0a2dd7a75a7ded8d7f
dotNETBeginner <ukanton1219@gmail.com>
2019-11-04 19:37:28 +0300

    /

---
+++ LAB - 9/zavd 3 - lab 9/задание 3 лаба 9/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace задание_3_лаба_9
+{
+    class Box <T>
+    {
+        public T[] SomeProps { get; set; }
+
+        public Box(int n,T[] someProps)
+        {
+            SomeProps = new T[n];
+            
+            for(int i=0;i<someProps.Length;i++)
+            { SomeProps[i] = someProps[i]; }
+        }
+
+        public void Display()
+        {
+            for(int i=0;i<SomeProps.Length;i++)
+            { Console.WriteLine($"{SomeProps[i].GetType().FullName}  -  {SomeProps[i]}"); }
+        }
+    }
+}
---
+++ LAB - 9/zavd 3 - lab 9/задание 3 лаба 9/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace задание_3_лаба_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string s;
+            string[] bS = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                s = Console.ReadLine();
+                bS[i] += " " + s;
+            }
+
+            Box<string> someBox = new Box<string>(n, bS);
+
+            bS = Console.ReadLine().Split(" ");
+
+            Swap<string>(someBox,int.Parse(bS[0]), int.Parse(bS[1]));
+        }
+
+        public static void Swap<T>(Box<T> Props, int i, int j)
+        {
+            T t = Props.SomeProps[i];
+            Props.SomeProps[i] = Props.SomeProps[j];
+            Props.SomeProps[j] = t;
+
+            Props.Display();
+        }
+    }
+}
