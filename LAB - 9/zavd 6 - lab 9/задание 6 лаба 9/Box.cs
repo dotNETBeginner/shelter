27a379b07caef03e67e888e16346a7bdb97c0e85
dotNETBeginner <ukanton1219@gmail.com>
2019-11-05 19:31:04 +0300

    6 задание 9 лаба

---
+++ LAB - 9/zavd 6 - lab 9/задание 6 лаба 9/Box.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace задание_6_лаба_9
+{
+    class Box<T> where T : IComparable
+
+    {
+        List<T> SomeProps = new List<T>();
+
+        public Box(T[] bS)
+        {
+            for (int i = 0; i < bS.Length; i++)
+            {
+                SomeProps.Add(bS[i]);
+            }
+        }
+
+        public int CompareValues(T[] someValue)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < SomeProps.Count; i++)
+            {
+                    if (someValue[0].CompareTo(SomeProps[i]) < 0)
+                    {
+                        counter++;
+                        continue;
+                    }
+            }
+
+            return counter;
+        }
+
+    }
+}
---
+++ LAB - 9/zavd 6 - lab 9/задание 6 лаба 9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+namespace задание_6_лаба_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double s;
+            double[] bS = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                s = double.Parse(Console.ReadLine());
+                bS[i] = s;
+            }
+
+            Box<double> someBox = new Box<double>(bS);
+
+            double[] S = new double[1];
+            S[0] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(someBox.CompareValues(S));
+        }
+    }
+}
