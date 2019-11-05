5c4049d80e978e2adc12bd3762568d113cbbbbef
dotNETBeginner <ukanton1219@gmail.com>
2019-11-05 21:51:22 +0300

    7 задание 9 лаба

---
+++ LAB - 9/zavd 7 - lab 9/7 задание 9 лаба/Box.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7_задание_9_лаба
+{
+    class Box<T> where T : IComparable
+    {
+        T[] SomeProps;
+
+        public Box() { SomeProps = new T[0]; }
+
+        public void Add(T element)
+        {
+            int n = SomeProps.Length;
+
+            n++;
+            Array.Resize(ref SomeProps, n);
+
+            for (int i = n - 1; i < n; i++)
+            { SomeProps[i] = element; }
+        }
+
+        public void Remove(int indx)
+        {
+            int n = SomeProps.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i == indx)
+                {
+                    for (int j = i; j < n - 1; j++)
+                        SomeProps[j] = SomeProps[j + 1];
+                    n--;
+                    Array.Resize(ref SomeProps,n);
+                }
+            }
+        }
+
+        public bool Contains(T element)
+        {
+            bool IsContains = false;
+
+            for(int i = 0; i < SomeProps.Length;i++)
+            {
+                if(element.CompareTo(SomeProps[i]) == 0)
+                { IsContains = true; }
+            }
+
+            return IsContains;
+        }
+
+        public void Swap(int indx1, int indx2)
+        {
+            T t = SomeProps[indx1];
+            SomeProps[indx1] = SomeProps[indx2];
+            SomeProps[indx2] = t;
+        }
+
+        public void Greater(T element)
+        {
+            int c = 0;
+            for(int i=0;i<SomeProps.Length;i++)
+            {
+                if(element.CompareTo(SomeProps[i]) < 0)
+                { c++; }
+            }
+
+            Console.WriteLine(c);
+        }
+
+        public void Max()
+        {
+            T max = SomeProps[0];
+            for(int i=0;i<SomeProps.Length;i++)
+            {
+                if(max.CompareTo(SomeProps[i]) < 0)
+                { max = SomeProps[i]; }
+            }
+
+            Console.WriteLine(max);
+        }
+
+        public void Min()
+        {
+            T min = SomeProps[0];
+            for (int i = 0; i < SomeProps.Length; i++)
+            {
+                if (min.CompareTo(SomeProps[i]) > 0)
+                { min = SomeProps[i]; }
+            }
+
+            Console.WriteLine(min);
+        }
+
+        public void Display()
+        {
+            for(int i=0;i<SomeProps.Length;i++)
+            { Console.WriteLine(SomeProps[i]); }
+        }
+    }
+}
---
+++ LAB - 9/zavd 7 - lab 9/7 задание 9 лаба/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _7_задание_9_лаба
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split(" ");
+
+            Box<string> someBox = new Box<string>();
+
+            while(command[0] != "END")
+            {
+                if(command[0] == "Add" && command.Length == 2)
+                { someBox.Add(command[1]); }
+
+                else if (command[0] == "Remove" && command.Length == 2)
+                { someBox.Remove(int.Parse(command[1])); }
+
+                else if (command[0] == "Contains" && command.Length == 2)
+                { Console.WriteLine(someBox.Contains(command[1])); }
+
+                else if (command[0] == "Swap" && command.Length == 3)
+                { someBox.Swap(int.Parse(command[1]),int.Parse(command[2])); }
+
+                else if (command[0] == "Greater" && command.Length == 2)
+                { someBox.Greater(command[1]); }
+
+                else if (command[0] == "Max" && command.Length == 1)
+                { someBox.Max(); }
+
+                else if (command[0] == "Min" && command.Length == 1)
+                { someBox.Min(); }
+
+                else if (command[0] == "Print" && command.Length == 1)
+                { someBox.Display(); }
+
+                else { Console.WriteLine("Invalid command!"); }
+
+                command = Console.ReadLine().Split(" ");
+            }
+
+        }
+    }
+}
