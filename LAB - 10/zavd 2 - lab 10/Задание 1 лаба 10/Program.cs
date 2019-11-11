ef6e1721d06b3d967ae9425a32ce6adc66660618
dotNETBeginner <ukanton1219@gmail.com>
2019-11-11 21:03:50 +0300

    2 задание 10 лаба

---
+++ LAB - 10/zavd 2 - lab 10/Задание 1 лаба 10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+namespace Задание_1_лаба_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> someBody = new ListyIterator<string>();
+
+            string[] command = Console.ReadLine().Split(" ");
+
+            if (command[0] == "Create" && command.Length > 1)
+            { someBody.Create(command); }
+            
+            while(command[0] != "END")
+            {
+                command = Console.ReadLine().Split(" ");
+                try
+                {
+                    if (command[0] == "Move")
+                    { Console.WriteLine(someBody.Move()); }
+                    else if (command[0] == "HasNext")
+                    { Console.WriteLine(someBody.HasNext()); }
+                    else if (command[0] == "Print")
+                    { someBody.Print(); }
+                    else if (command[0] == "PrintAll")
+                    {
+                        IEnumerator ie = someBody.PrintAll();
+                        while(ie.MoveNext())
+                        {
+                            string item = (string)ie.Current;
+                            Console.Write(item + " ");
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid Operation!");
+                }
+            }
+        }
+    }
+}
