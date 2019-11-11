89ee187884520a5fd02bab50b6211b8d706d1859
dotNETBeginner <ukanton1219@gmail.com>
2019-11-11 20:14:30 +0300

    1 задание 10 лаба

---
+++ LAB - 10/zavd 1 - lab 10/Задание 1 лаба 10/ListyIterator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_1_лаба_10
+{
+    class ListyIterator <T>
+    {
+        private int currIndex;
+        public T[] someProps { get; set; }
+
+        public ListyIterator() { }
+
+        public bool Move()
+        {
+            if(HasNext())
+            { currIndex++; return true; }
+            else { return false; }
+        }
+
+        public bool HasNext()
+        {
+            if ((currIndex + 1) < someProps.Length)
+            { return true; }
+            else { return false; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(someProps[currIndex]);
+        }
+
+        public void Create(T[] array)
+        {
+            int j = 1;
+            someProps = new T[array.Length - 1];
+
+            for (int i = 0; i < someProps.Length; i++)
+            { someProps[i] = array[j]; j++; }
+
+            currIndex = 0;
+        }
+    }
+}
---
+++ LAB - 10/zavd 1 - lab 10/Задание 1 лаба 10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Задание_1_лаба_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> someBody = new ListyIterator<string>();
+
+            string[] command = Console.ReadLine().Split(" ");
+
+            if (command[0] == "Create" && command.Length > 1)
+            { someBody.Create(command); }
+            
+            while(command[0] != "END")
+            {
+                command = Console.ReadLine().Split(" ");
+                try
+                {
+                    if (command[0] == "Move")
+                    { someBody.Move(); }
+                    else if (command[0] == "HasNext")
+                    { Console.WriteLine(someBody.HasNext()); }
+                    else if (command[0] == "Print")
+                    { someBody.Print(); }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid Operation!");
+                }
+            }
+        }
+    }
+}
