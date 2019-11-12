3651588cbf47a8a243c12a60df7d8cc4b5adba3a
dotNETBeginner <ukanton1219@gmail.com>
2019-11-12 14:56:08 +0300

    4 задание 10 лаба

---
+++ LAB - 10/zavd 4 - lab 10/задание 4 лаба 10/Lake.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace задание_4_лаба_10
+{
+    class Lake
+    {
+        public int[] StonesInLake { get; set; }
+
+        public Lake(int[] stones) 
+        {
+            StonesInLake = new int[stones.Length];
+
+            for (int i = 0; i < StonesInLake.Length; i++)
+            { StonesInLake[i] = stones[i]; }
+        }
+
+        public IEnumerable<int> GetEnumerator()
+        {
+            for(int i=0;i<StonesInLake.Length;i++)
+            {
+                if(StonesInLake[i] % 2 != 0)
+                { yield return StonesInLake[i]; }
+            }
+
+            for (int i = StonesInLake.Length - 1; i >= 0; i--)
+            {
+                if (StonesInLake[i] % 2 == 0)
+                { yield return StonesInLake[i]; }
+            }
+        }
+    }
+}
---
+++ LAB - 10/zavd 4 - lab 10/задание 4 лаба 10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace задание_4_лаба_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] command = Console.ReadLine().Split(" ");
+
+            int[] stones = new int[command.Length];
+
+            for(int i=0; i<stones.Length;i++)
+            { stones[i] = int.Parse(command[i]); }
+
+            Lake someLake = new Lake(stones);
+
+            foreach(int stone in someLake.GetEnumerator())
+            { Console.Write(stone + " "); }
+        }
+    }
+}
