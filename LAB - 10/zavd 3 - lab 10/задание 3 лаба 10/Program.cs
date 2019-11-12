a1edc0ffd7e6626e8e9f2c2e2b621d1a4ba4f3f3
dotNETBeginner <ukanton1219@gmail.com>
2019-11-12 14:28:54 +0300

    3 задание 10 лаба

---
+++ LAB - 10/zavd 3 - lab 10/задание 3 лаба 10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace задание_3_лаба_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> someStack = new Stack<int>();
+
+            string[] command = Console.ReadLine().Split(" ");
+
+            if(command[0] == "Push" && command.Length > 1)
+            {
+                int j = 1;
+                int[] s = new int[command.Length - 1];
+                for(int i = 0; i < s.Length; i++)
+                { s[i] = int.Parse(command[j]); j++; }
+                someStack.Push(s);
+
+            }
+
+            while(command[0] != "END")
+            {
+                command = Console.ReadLine().Split(" ");
+                if (command[0] == "Push" && command.Length > 1)
+                {
+                    int j = 1;
+                    int[] s = new int[command.Length - 1];
+                    for (int i = 0; i < s.Length; i++)
+                    { s[i] = int.Parse(command[j]); j++; }
+                    someStack.Push(s);
+                }
+
+                else if(command[0] == "Pop")
+                { someStack.Pop(); }
+            }
+
+            foreach(int i in someStack.PrintAll())
+            { Console.WriteLine(i); }
+        }
+    }
+}
---
+++ LAB - 10/zavd 3 - lab 10/задание 3 лаба 10/Stack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace задание_3_лаба_10
+{
+    class Stack<T>
+    {
+        private T[] someProps;
+        public int CurrIndex { get; set; }
+        public T[] SomeProps
+        {
+            get { return someProps; }
+            set { someProps = value; } 
+        }
+
+        public Stack() { }
+
+        public void Push(T[] array)
+        {
+            int n = 0;
+
+            if(someProps != null)
+            { n = someProps.Length;}
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                n++;
+                Array.Resize(ref someProps,n);
+                SomeProps[SomeProps.Length-1] = array[i];
+            }
+        }
+
+        public void Pop()
+        {
+            int n = SomeProps.Length;
+
+            if (n == 0)
+            { Console.WriteLine("No elements"); }
+            else
+            {
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (i == (n - 1))
+                    {
+                        for (int j = i; j < n - 1; j++)
+                            SomeProps[j] = SomeProps[j + 1];
+                        n--;
+                        Array.Resize(ref someProps, n);
+                        break;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<T> PrintAll()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for(int j = someProps.Length-1; j >= 0; j--)
+                { yield return someProps[j]; }
+            }
+        }
+    }
+}
