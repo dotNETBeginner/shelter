56fc37a2f6620fc14bf775c4cb5c1e9c8fdc5a3e
dotNETBeginner <ukanton1219@gmail.com>
2019-11-18 18:26:39 +0300

    7 задание 10 лаба

---
+++ LAB - 10/zavd 7 - lab 10/Задание 7 лаба 10/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Задание_7_лаба_10
+{
+    class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override int GetHashCode()
+        {
+            int result = 0;
+
+            for(int i=0; i<Name.Length; i++)
+            { result += Name[i]; }
+
+            result += Age;
+
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person n = (Person)obj;
+
+            return this.GetHashCode() == n.GetHashCode() && (this.GetHashCode() - this.Age) == (n.GetHashCode() - n.Age) && this.Age == n.Age;
+        }
+
+        public void Display() => Console.WriteLine($"{Name} {Age}");
+    }
+}
---
+++ LAB - 10/zavd 7 - lab 10/Задание 7 лаба 10/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Задание_7_лаба_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> sortedSet = new List<Person>();
+            List<Person> hashSet = new List<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            string[] command = { "" }; 
+
+            for(int i=0; i<n; i++)
+            {
+                command = Console.ReadLine().Split(" ");
+
+                sortedSet.Add(new Person(command[0],int.Parse(command[1])));
+                hashSet.Add(new Person(command[0], int.Parse(command[1])));
+            }
+
+            for(int i=0; i<hashSet.Count; i++)
+            {
+                for(int j=0; j<hashSet.Count; j++)
+                {
+                    if (j != i)
+                    {
+                        if (hashSet[j].Equals(hashSet[i]))
+                        { hashSet.Remove(hashSet[i]); }
+                    }
+                }
+            }
+
+            for (int i = 0; i < sortedSet.Count; i++)
+            {
+                for (int j = 0; j < sortedSet.Count; j++)
+                {
+                    if (j != i)
+                    {
+                        if (sortedSet[j].Equals(sortedSet[i]))
+                        { sortedSet.Remove(sortedSet[i]); }
+                    }
+                }
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+        }
+    }
+}
