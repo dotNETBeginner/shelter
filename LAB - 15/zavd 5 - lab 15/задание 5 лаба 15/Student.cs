87b82b1435700a559de4ba7a7b0d27cbc159b3bd
dotNETBeginner <ukanton1219@gmail.com>
2019-12-02 19:46:59 +0300

    .

---
+++ LAB - 15/zavd 5 - lab 15/задание 5 лаба 15/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace задание_5_лаба_15
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Email { get; set; }
+
+        public Student(string firstNane, string secondName, string email)
+        {
+            FirstName = firstNane;
+            SecondName = secondName;
+            Email = email;
+        }
+
+        public void Display() => Console.WriteLine($"{FirstName} {SecondName}");
+    }
+}
+
+
