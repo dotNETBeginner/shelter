f271e94914a4588010990d921af8b7bc907c9f1a
dotNETBeginner <ukanton1219@gmail.com>
2019-12-02 18:51:23 +0300

    /

---
+++ LAB - 15/zavd 4 - lab 15/4 задание 15 лаба/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _4_задание_15_лаба
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> someStudents = new List<Student>();
+
+            string[] command = { "" };
+
+            while (command[0] != "END")
+            {
+                command = Console.ReadLine().Split(" ");
+
+                if (command.Length == 2)
+                { someStudents.Add(new Student(command[0], command[1])); }
+            }
+
+            var selectedStudents = someStudents.OrderBy(s=>s.SecondName).ThenByDescending(s=>s.FirstName);
+
+            foreach (Student std in selectedStudents)
+            { std.Display(); }
+        }
+    }
+}
---
+++ LAB - 15/zavd 4 - lab 15/4 задание 15 лаба/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4_задание_15_лаба
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+
+        public Student(string firstName, string secondName)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+        }
+
+        public void Display() => Console.WriteLine($"{FirstName} {SecondName}");
+    }
+}
+
+
