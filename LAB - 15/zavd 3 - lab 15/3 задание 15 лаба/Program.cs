23346535a27ad7e68ef617f262aef9b6eebea98c
dotNETBeginner <ukanton1219@gmail.com>
2019-12-02 18:12:11 +0300

    /

---
+++ LAB - 15/zavd 3 - lab 15/3 задание 15 лаба/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _3_задание_15_лаба
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> someStudent = new List<Student>();
+
+            string[] command = { "" };
+
+            while (command[0] != "END")
+            {
+                command = Console.ReadLine().Split(" ");
+
+                if (command.Length == 3)
+                { someStudent.Add(new Student(command[0], command[1], int.Parse(command[2]))); }
+            }
+
+            var selectedStudents = from s in someStudent
+                                   where s.Age >= 18 && s.Age <= 24
+                                   select s;
+
+            foreach(Student s in selectedStudents)
+            { s.Display(); }
+        }
+    }
+}
---
+++ LAB - 15/zavd 3 - lab 15/3 задание 15 лаба/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3_задание_15_лаба
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public int Age { get; set; }
+
+        public Student(string firstNane, string secondName, int age)
+        {
+            FirstName = firstNane;
+            SecondName = secondName;
+            Age = age;
+        }
+
+        public void Display() => Console.WriteLine($"{FirstName} {SecondName} {Age}");
+    }
+}
+
