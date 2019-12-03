7672a7435ffadc2af4639a5fb7b656b936cf53ec
dotNETBeginner <ukanton1219@gmail.com>
2019-12-03 18:30:30 +0300

    10 задание лаба 15

---
+++ LAB - 15/zavd 10 - lab 15/задание 10 лаба 15/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace задание_10_лаба_15
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public string SecondName { get; set; }
+
+        public int GroupName { get; set; }
+
+        public Person(string name, string secondName, int groupName)
+        {
+            Name = name;
+            SecondName = secondName;
+            GroupName = groupName;
+        }
+    }
+}
---
+++ LAB - 15/zavd 10 - lab 15/задание 10 лаба 15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace задание_10_лаба_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> somePersons = new List<Person>();
+
+            string[] command = { "" };
+
+            while(command[0] != "END")
+            {
+                command = Console.ReadLine().Split(" ");
+
+                if (command.Length == 3)
+                { somePersons.Add(new Person(command[0], command[1],int.Parse(command[2]))); }
+            }
+
+            var sortedPersons = somePersons.OrderBy(s => s.GroupName);
+
+            var personsGroup = from p in sortedPersons
+                               group p by p.GroupName;
+
+            foreach(IGrouping<int,Person> p in personsGroup)
+            {
+                Console.Write($"{p.Key} - ");
+                foreach(var g in p)
+                { Console.Write($"{g.Name} {g.SecondName}, "); }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
