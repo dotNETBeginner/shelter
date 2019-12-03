3ee58e3a1383a86439909b2c0c4c471cfbe5360c
dotNETBeginner <ukanton1219@gmail.com>
2019-12-03 17:14:53 +0300

    7 задание 15 лаба

---
+++ LAB - 15/zavd 7 - lab 15/задание 7 лаба 15/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace задание_7_лаба_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> someStudent = new List<Student>();
+
+            string[] command = { "" };
+            string[] marks = { "" };
+
+            while (command[0] != "END")
+            {
+                command = Console.ReadLine().Split(" ");
+
+                if (command[0] != "END")
+                {
+                    int[] intMarks = new int[command.Length - 2];
+                    StringToInt(intMarks, command);
+
+                    if (intMarks.Length + 2 == command.Length)
+                    { someStudent.Add(new Student(command[0], command[1], intMarks)); }
+                }
+
+            }
+
+            var selectedStudent = from s in someStudent
+                                  where s.Mark.Any(m => m == 6)
+                                  select s;
+
+            foreach (Student s in selectedStudent)
+            { s.Display(); }
+
+        }
+
+        static void StringToInt(int[] a, string[] b)
+        {
+            int q = 2;
+            for (int i = 0; i < a.Length; i++)
+            { a[i] = int.Parse(b[q]); q++; }
+        }
+
+    }
+}
