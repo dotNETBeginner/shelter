ec9d67b4fc51c59ff5a9d1d0aba830fa57286a80
dotNETBeginner <ukanton1219@gmail.com>
2019-12-03 17:56:53 +0300

    задание 9 лаба 15

---
+++ LAB - 15/zavd 9 - lab 15/задание 9 лаба 15/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace задание_9_лаба_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> someStudent = new List<Student>();
+
+            string[] command = { "" };
+            string[] marks = { "" };
+
+            while (command[0] != "END")
+            {
+                command = Console.ReadLine().Split(" ");
+
+                if (command[0] != "END")
+                {
+                    int[] intMarks = new int[command.Length - 1];
+                    StringToInt(intMarks, command);
+
+                    if (intMarks.Length + 1 == command.Length)
+                    { someStudent.Add(new Student(command[0], intMarks)); }
+                }
+
+            }
+
+            var selectedStudent = from s in someStudent
+                                  where s.FacultyName.EndsWith("14") || s.FacultyName.EndsWith("15")
+                                  select s;
+
+            foreach (Student s in selectedStudent)
+            { s.Display(); }
+
+        }
+
+        static void StringToInt(int[] a, string[] b)
+        {
+            int q = 1;
+            for (int i = 0; i < a.Length; i++)
+            { a[i] = int.Parse(b[q]); q++; }
+        }
+
+    }
+}
---
+++ LAB - 15/zavd 9 - lab 15/задание 9 лаба 15/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace задание_9_лаба_15
+{
+    class Student
+    {
+        public string FacultyName { get; set; }
+        public int[] Mark { get; set; }
+
+        public Student(string facultyName, int[] mark)
+        {
+            FacultyName = facultyName;
+
+            Mark = new int[mark.Length];
+
+            for (int i = 0; i < Mark.Length; i++)
+            { Mark[i] = mark[i]; }
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < Mark.Length; i++)
+            { Console.Write($"{Mark[i]} "); }
+            Console.WriteLine();
+        }
+    }
+}
+
