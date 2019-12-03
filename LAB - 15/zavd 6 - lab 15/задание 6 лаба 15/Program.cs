b8cd3ec2ed82ea6bfa3c78019bc5bdf80ff776ab
dotNETBeginner <ukanton1219@gmail.com>
2019-12-03 16:37:29 +0300

    6 задание 15 лаба

---
+++ LAB - 15/zavd 6 - lab 15/задание 6 лаба 15/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace задание_6_лаба_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> someStudent = new List<Student>();
+
+            string[] command = { "" };
+
+            while (command[0] != "END")
+            {
+                command = Console.ReadLine().Split(" ");
+
+                if (command.Length == 3)
+                { someStudent.Add(new Student(command[0], command[1], command[2])); }
+            }
+
+            var selectedStudent = from s in someStudent
+                                  where s.Phone.StartsWith("02") || s.Phone.StartsWith("+3592")
+                                  select s;
+
+            foreach (Student s in selectedStudent)
+            { s.Display(); }
+        }
+    }
+}
---
+++ LAB - 15/zavd 6 - lab 15/задание 6 лаба 15/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace задание_6_лаба_15
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Phone { get; set; }
+
+        public Student(string firstNane, string secondName, string phone)
+        {
+            FirstName = firstNane;
+            SecondName = secondName;
+            Phone = phone;
+        }
+
+        public void Display() => Console.WriteLine($"{FirstName} {SecondName}");
+    }
+}
+
