0c8b646931ae61082751a0345967680a14af1a43
dotNETBeginner <ukanton1219@gmail.com>
2019-12-12 01:04:12 +0300

    zavd 2 - lab 13

---
+++ LAB - 13/zavd 1 - lab 13/1 задание лаба 13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1_задание_лаба_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = null;
+
+            Regex someReg = new Regex(@"\b[A-Z]{1}[a-z]*\s[A-Z]{1}[a-z]*\b");
+
+            while (s != "END")
+            {
+                s = Console.ReadLine();
+
+                if(s != "END")
+                {
+
+                    MatchCollection someMatches = someReg.Matches(s);
+
+                    if (someMatches.Count > 0)
+                    {
+                        foreach (Match match in someMatches)
+                        { Console.WriteLine(match.Value); }
+                    }
+                    else
+                    { Console.WriteLine("No mathces!"); }
+
+                }
+            }
+        }
+    }
+}
---
+++ LAB - 13/zavd 2 - lab 13/задание 2 лаба 13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace задание_2_лаба_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = null;
+
+            Regex someReg = new Regex(@"\+359(?<grp1>[- ])[0-9]\k<grp1>[0-9]{3}\k<grp1>[0-9]{4}\b");
+
+            while(s != "END")
+            {
+                s = Console.ReadLine();
+
+                if (s != "END")
+                {
+                    MatchCollection someMatches = someReg.Matches(s);
+
+                    if (someMatches.Count > 0)
+                    {
+                        foreach (Match match in someMatches)
+                        { Console.WriteLine(match.Value); }
+                    }
+                    else
+                    { Console.WriteLine("No matches!"); }
+                }
+            }
+        }
+    }
+}
