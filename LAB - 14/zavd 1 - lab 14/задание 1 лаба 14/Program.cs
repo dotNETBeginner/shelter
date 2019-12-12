9f51f58fb8149abb51d1837add29051f284de070
dotNETBeginner <ukanton1219@gmail.com>
2019-12-12 21:07:10 +0300

    zavd 1 - lab 14

---
+++ LAB - 14/zavd 1 - lab 14/задание 1 лаба 14/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace задание_1_лаба_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int n = s.Length/2;
+
+            char[] cs = new char[s.Length];
+
+            for(int i = 0; i < s.Length; i++)
+            { cs[i] = s[i]; }
+
+                int j = s.Length-1;
+
+                for(int i = 0; i < n; i++)
+                {
+                    char t = cs[j];
+                    cs[j] = cs[i];
+                    cs[i] = t;
+                    j--;
+                }
+
+
+            for(int i = 0; i < s.Length; i++)
+            { Console.Write(cs[i]); }
+        }
+    }
+}
