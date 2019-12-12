37ed4d1ebf03c6de49011480b15284c792e0d2cc
dotNETBeginner <ukanton1219@gmail.com>
2019-12-12 22:13:31 +0300

    zavd 3 - lab 14

---
+++ LAB - 14/zavd 3 - lab 14/задание 3 лаба 15/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace задание_3_лаба_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email = Console.ReadLine();
+
+            string[] sEmail = email.Split("@");
+
+            string replacmentString = null;
+
+            for(int i = 0; i < sEmail[0].Length; i++)
+            { replacmentString += '*'; }
+
+            sEmail[0] = replacmentString;
+
+            string t = null;
+
+            t += $"{sEmail[0]}@{sEmail[1]}";
+
+            string[] c = Console.ReadLine().Split(" ");
+
+            for(int i = 0; i < c.Length; i++)
+            {
+                if (c[i] == email)
+                { c[i] = t; }
+            }
+
+            for(int i = 0; i < c.Length; i++)
+            { Console.Write($"{c[i]} "); }
+
+            //char[] cc = new char[c.Length];
+
+            //for (int i = 0; i < c.Length; i++)
+            //{ cc[i] = c[i]; }
+
+            //int j = 0;
+
+            //for(int i = 0; i < cc.Length;i++)
+            //{
+            //        if (cc[i] == email[j])
+            //        {
+            //            cc[i] = '*';
+            //            j++;
+                      
+            //        }
+            //}
+
+            //for(int i = 0; i < cc.Length; i++)
+            //{ Console.Write(cc[i]); }
+
+        }
+    }
+}
