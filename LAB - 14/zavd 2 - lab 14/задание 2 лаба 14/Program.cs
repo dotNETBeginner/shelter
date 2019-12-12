5d6e2e52d76907ca007f843e07dbb7c48e96bb06
dotNETBeginner <ukanton1219@gmail.com>
2019-12-12 21:25:23 +0300

    zavd 2 - lab 14

---
+++ LAB - 14/zavd 2 - lab 14/задание 2 лаба 14/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace задание_2_лаба_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int n = 20; 
+
+            for(int i = 0; i < n; i++)
+            {
+                if (i < s.Length)
+                {
+                    if (s[i] == '"')
+                    {
+                        i++;
+                        n++;
+                    }
+                     Console.Write(s[i]);  
+                }
+
+                else if(i >= s.Length)
+                {
+                    Console.Write("*");
+                }
+            }
+        }
+    }
+}
