2c8e7db015f1fc2a18bcc37ec3a581eb0b37c79e
dotNETBeginner <ukanton1219@gmail.com>
2019-12-12 23:30:57 +0300

    zavd 4 - lab 14

---
+++ LAB - 14/zavd 4 - lab 14/задание 4 лаба 14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace задание_4_лаба_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string[]> someS = new List<string[]>();
+
+            string p = Console.ReadLine();
+
+            string[] s = Console.ReadLine().Split('?','.','!');
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                string[] m = s[i].Split(" ");
+                someS.Add(m);
+            }
+
+            for (int q = 0; q < s.Length; q++)
+            {
+                for (int i = 0; i < someS.Count; i++)
+                {
+                    if (i == q)
+                    {
+                        for (int j = 0; j < someS[i].Length; j++)
+                        {
+                            if(someS[i][j] == p)
+                            Console.WriteLine(s[i]);
+                        }
+                        break;
+                    }
+                }
+            }
+
+
+        }
+    }
+}
